27b6beb1cde5fe1a7a7a2cfd97b514d8c51d3b6e
Schuyler Schwafel <saschwafel@gmail.com>
2014-12-22 22:05:11 -0800

    Finishing up music updater

--- music_updater/music_updater.py
+++ music_updater/music_updater.py
@@ -2,19 +2,16 @@
 
 #Run Rsync to update local music backup from server
 
-import sys
-import os
 #import subprocess32 as subprocess
+import os
 import subprocess
+import sys
 
 path = os.getcwd()
 
 print 'Your current working dir is {}'.format(path)
 
 def rsync_music_folder(x):
-
-    #print type(x)
-    #print x
 
     print 'Now Rsyncing your music directory from server...\n'
 
@@ -28,6 +25,7 @@ def rsync_music_folder(x):
         print subprocess.call(['rsync', '-rtvu', '--progress', '--delete', 'wizard:/home/schuyler/Downloads/MusicBackup/', '/media/Music/MusicBackup'])
     else:
         print 'exiting...'
+
 #alias updatemusic="rsync -rtvu --progress --delete wizard:/home/schuyler/Downloads/MusicBackup/ /media/sschwafel/Music/MusicBackup"
 user_input = raw_input('To update music with default music folder path ( /media/Music/MusicBackup ) please enter y, to enter a new path, enter n\n')
 
@@ -35,20 +33,22 @@ if user_input.lower() == 'y':
     print 'Your music folder is located here {}'.format('/media/Music/MusicBackup')
     backupdir_path = '/media/Music/MusicBackup'
 
+    rsync_music_folder(backupdir_path)
+
+#    sys.exit()
+
 else:
     
     new_user_path = raw_input('What path would you like to use instead? ')
 
     if os.path.exists(new_user_path):
         print "That path exists"
-        #
         backupdir_path = new_user_path
+        #Call Rsync function on the provided path
         rsync_music_folder(backupdir_path)
 
-        #call rsync function here
     else: 
-        print "That path is nonexistant"
+        print "That path is nonexistant\n\nPlease try running the program again"
         sys.exit()
 
-#put this into a function
 
